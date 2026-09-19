7c2cdea7754f62c8b06dc8930d8fd5a024a26966
sintjuri <sintjuri@yandex.ru>
2026-09-19 19:43:49 +0300

    Block worktrees in ydbdoc-review

---
+++ .cursor/hooks/forbid-worktrees.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+printf "{\\"permission\\":\\"deny\\",\\"user_message\\":\\"В ydbdoc-review запрещены worktree. Работайте только в main.\\",\\"agent_message\\":\\"Do not create or use Git worktrees in ydbdoc-review. Work directly in main.\\"}"
